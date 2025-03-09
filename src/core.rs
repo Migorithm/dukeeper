b785429279d99401c8b93c6005124011d87532ca
Migo <saka90030@gmail.com>
2025-03-09 21:08:21 +0400

    watcher implementd

--- src/core.rs
+++ src/core.rs
@@ -1,41 +1,41 @@
 use std::collections::HashMap;
 
+use crate::node::Lease;
+
 #[derive(Default)]
 pub struct ConsistentCore {
-    nodes: HashMap<GroupId, Vec<Node>>,
+    nodes: HashMap<Lease, Vec<Node>>,
 }
 
 impl ConsistentCore {
-    pub(crate) fn register_lease(
-        &mut self,
-        group_id: GroupId,
-        node_id: u64,
-        ttl: u64,
-    ) -> Result<(), String> {
-        let nodes = self.nodes.entry(group_id).or_insert(vec![]);
+    pub(crate) fn register_lease(&mut self, lease: Lease, node_id: u64) -> Result<(), String> {
+        let nodes = self.nodes.entry(lease).or_insert(vec![]);
 
         nodes.push(Node {
             id: node_id,
             role: Role::Controller,
-            ttl,
         });
 
         if nodes.iter().filter(|n| n.role == Role::Controller).count() > 1 {
             return Err("Controller already exists".to_string());
         }
+        Ok(())
+    }
 
+    pub(crate) fn watch(&mut self, group_id: &str, node_id: u64) -> Result<(), String> {
+        let nodes = self.nodes.get_mut(group_id).ok_or("Group not found")?;
+        nodes.push(Node {
+            id: node_id,
+            role: Role::Follower,
+        });
         Ok(())
     }
 }
-
-#[derive(Hash, PartialEq, Eq, Clone)]
-pub struct GroupId(String);
 
 #[derive(Debug)]
 pub struct Node {
     id: u64,
     role: Role,
-    ttl: u64,
 }
 
 #[derive(Debug, PartialEq)]
@@ -48,15 +48,15 @@ enum Role {
 fn test_register_lease() {
     let mut core = ConsistentCore::default();
 
-    let group_id = GroupId("group1".to_string());
-    assert!(core.register_lease(group_id, 1, 10).is_ok());
+    let group_id = Lease::new("group1", 300);
+    assert!(core.register_lease(group_id, 1).is_ok());
 }
 
 #[test]
 fn test_register_lease_twice_return_err() {
     let mut core = ConsistentCore::default();
 
-    let group_id = GroupId("group1".to_string());
-    assert!(core.register_lease(group_id.clone(), 1, 10).is_ok());
-    assert!(core.register_lease(group_id.clone(), 2, 10).is_err());
+    let group_id = Lease::new("group1", 300);
+    assert!(core.register_lease(group_id.clone(), 1).is_ok());
+    assert!(core.register_lease(group_id.clone(), 2).is_err());
 }
--- src/node.rs
+++ src/node.rs
@@ -1,1 +1,43 @@
+use std::borrow::Borrow;
 
+#[derive(Clone)]
+pub struct Lease {
+    group_id: String,
+    ttl: u64,
+}
+
+impl Lease {
+    pub fn new(group_id: &str, ttl: u64) -> Self {
+        Lease {
+            group_id: group_id.into(),
+            ttl,
+        }
+    }
+}
+
+impl PartialEq for Lease {
+    fn eq(&self, other: &Self) -> bool {
+        self.group_id == other.group_id
+    }
+}
+
+impl PartialEq<str> for Lease {
+    fn eq(&self, other: &str) -> bool {
+        self.group_id == other
+    }
+}
+
+impl Borrow<str> for Lease {
+    fn borrow(&self) -> &str {
+        &self.group_id
+    }
+}
+
+// Required to use Lease in a HashMap
+impl Eq for Lease {}
+
+impl std::hash::Hash for Lease {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        self.group_id.hash(state);
+    }
+}
