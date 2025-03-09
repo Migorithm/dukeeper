9c458d6683d55a5991907a8ca8c2d39df2490a59
Migo <saka90030@gmail.com>
2025-03-09 21:10:38 +0400

    test cases

--- src/core.rs
+++ src/core.rs
@@ -24,6 +24,11 @@ impl ConsistentCore {
 
     pub(crate) fn watch(&mut self, group_id: &str, node_id: u64) -> Result<(), String> {
         let nodes = self.nodes.get_mut(group_id).ok_or("Group not found")?;
+
+        if nodes.iter().find(|n| n.id == node_id).is_some() {
+            return Err("Node already exists".to_string());
+        }
+
         nodes.push(Node {
             id: node_id,
             role: Role::Follower,
@@ -59,4 +64,29 @@ fn test_register_lease_twice_return_err() {
     let group_id = Lease::new("group1", 300);
     assert!(core.register_lease(group_id.clone(), 1).is_ok());
     assert!(core.register_lease(group_id.clone(), 2).is_err());
+}
+
+#[test]
+fn test_watch() {
+    let mut core = ConsistentCore::default();
+
+    let group_id = Lease::new("group1", 300);
+    assert!(core.register_lease(group_id.clone(), 1).is_ok());
+    assert!(core.watch("group1", 2).is_ok());
+}
+
+#[test]
+fn test_watch_twice_return_err() {
+    let mut core = ConsistentCore::default();
+
+    let group_id = Lease::new("group1", 300);
+    assert!(core.register_lease(group_id.clone(), 1).is_ok());
+    assert!(core.watch("group1", 2).is_ok());
+    assert!(core.watch("group1", 2).is_err());
+}
+
+#[test]
+fn test_watch_without_controller_return_err() {
+    let mut core = ConsistentCore::default();
+    assert!(core.watch("group1", 2).is_err());
 }
